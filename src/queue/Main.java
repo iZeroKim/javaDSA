2d3f2e7a18657368d31326e943bb43628dd386b7
iZeroKim <kimgichuru6@gmail.com>
2022-10-16 20:00:54 +0300

    Create Blog class and implement the comparable interface

--- src/queue/Main.java
+++ src/queue/Main.java
@@ -1,7 +1,37 @@
 package queue;
 
+
+class Blog implements Comparable<Blog>{
+    int id;
+    String title, author, publisher;
+    int numOfLines;
+
+    public Blog(int id, String title, String author, String publisher, int numOfLines) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.numOfLines = numOfLines;
+    }
+
+    @Override
+    public int compareTo(Blog blog) {
+        if(this.numOfLines > blog.numOfLines){
+            return 1;
+        } else if (this.numOfLines > blog.numOfLines){
+            return -1;
+        }else {
+            return 0;
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Queue Interface");
+        System.out.println("Queue Interface\n\n");
+
+        System.out.println("PriorityQueue Queue implementation");
+
+
     }
 }
